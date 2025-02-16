11748e1296e95f04e107083974cff5db30aa7524
manos <manos.akpujiha@gmail.com>
2025-02-16 00:28:16 -0500

    Refactor MongoDB connection to use environment-specific URIs and clean up port assignment

--- app.js
+++ app.js
@@ -3,26 +3,26 @@ const mongoose = require('mongoose');
 require('dotenv').config()
 const path = require('path');
 const counterRoutes = require('./routes/index');
-
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT;
 
-mongoose.connect('mongodb+srv://manosakpujiha:UrXx4YCTKpaANGFk@clubrecipee-cluster.1x1x0.mongodb.net/?retryWrites=true&w=majority&appName=clubrecipee-cluster' , {
-    dbName: 'counter',
-});
+const MONGODB_URI = process.env.VERCEL_ENV === 'production' 
+    ? process.env.MONGODB_URI_PROD
+    : process.env.MONGODB_URI_DEV;
 
+mongoose.connect(MONGODB_URI, {
+      dbName: 'counter',
+  });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
   console.log('Connected to MongoDB');
 });
 
-app.use(express.static(path.join(__dirname, 'public'))); // Serve static files
-
+app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
-
 app.use('/api', counterRoutes);
 
 app.listen(port, () => {
