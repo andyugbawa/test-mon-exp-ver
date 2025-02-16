6061aba1ff6bab1a4cf684b70cf45c000c67c6b6
manos <manos.akpujiha@gmail.com>
2025-02-16 01:09:40 -0500

    Comment out server listen call and port assignment to enhance flexibility for deployment

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ require('dotenv').config()
 const path = require('path');
 const counterRoutes = require('./routes/index');
 const app = express();
-const port = process.env.PORT;
+// const port = process.env.PORT;
 
 const MONGODB_URI = process.env.VERCEL_ENV === 'production' 
     ? process.env.MONGODB_URI_PROD
@@ -31,6 +31,8 @@ app.get('/', (req, res) => {
 });
 app.use('/api', counterRoutes);
 
-
+// app.listen(port, () => {
+//   console.log(`Server is running on port ${port}`);
+// });
 
 module.exports = app;
