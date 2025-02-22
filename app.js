6aa5e18981294e1417e0554166bc83f3b23239c2
andyugbawa <118132794+andyugbawa@users.noreply.github.com>
2025-02-22 02:23:23 -0500

    Update app.js

--- app.js
+++ app.js
@@ -21,6 +21,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
 app.use('/api', counterRoutes);
 app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port`);
+  // console.log(`Server is running on port`);
 });
 module.exports = app;
