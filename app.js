956346263f1eef5851af4a47670a28d882aaf6ce
andyugbawa <118132794+andyugbawa@users.noreply.github.com>
2025-02-22 00:17:11 -0500

    Update app.js

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ const app = express();
 const counterRoutes = require('./routes/index');
 
 // MongoDB Connection
-const MONGODB_URI = process.env.VERCEL_ENV === 'production' 
-    ? process.env.MONGODB_URI_PROD
-    : process.env.MONGODB_URI_PROD;
-    console.log(MONGODB_URI);
+// const MONGODB_URI = process.env.VERCEL_ENV === 'production' 
+//     ? process.env.MONGODB_URI_PROD
+//     : process.env.MONGODB_URI_PROD;
+//     console.log(MONGODB_URI);
 mongoose.connect("mongodb+srv://andyugbawa:utXW8UXhMNSXq8g4@cluster0.sfypk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", { dbName: 'counter' })
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => {
