8aa24b5b24f98242e7f532fb75f62d5122fc87eb
andy <andyugbawa@gmail.com>
2025-02-22 06:06:41 +0100

    new commit

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const counterRoutes = require('./routes/index');
 const MONGODB_URI = process.env.VERCEL_ENV === 'production' 
     ? process.env.MONGODB_URI_PROD
     : process.env.MONGODB_URI_PROD;
-    console.log(MONGODB_URI)
+    console.log(MONGODB_URI);
 mongoose.connect("mongodb+srv://andyugbawa:utXW8UXhMNSXq8g4@cluster0.sfypk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", { dbName: 'counter' })
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => {
