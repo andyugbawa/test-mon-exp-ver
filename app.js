5ff11f1e736f1a734432c8eb1851e817861c3abe
andy <andyugbawa@gmail.com>
2025-02-24 03:37:03 +0100

    new changes

--- app.js
+++ app.js
@@ -5,10 +5,12 @@ require('dotenv').config();
 const app = express();
 const counterRoutes = require('./routes/index');
 
+
+
 // MongoDB Connection
 const MONGODB_URI = process.env.VERCEL_ENV === 'production' 
     ? process.env.MONGODB_URI_PROD 
-    : process.env.MONGODB_URI_DEV;
+    : process.env.MONGODB_URI_PROD;
 mongoose.connect(MONGODB_URI, { dbName: 'counter' })
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => {
@@ -20,4 +22,8 @@ mongoose.connect(MONGODB_URI, { dbName: 'counter' })
 app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
 app.use('/api', counterRoutes);
+app.listen(3001,()=>{
+    console.log("LISTEN 3001 APP")
+})
 module.exports = app;
+
